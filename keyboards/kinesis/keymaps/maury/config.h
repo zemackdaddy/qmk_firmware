81bb236a81aebd5376fcddbed837fcfdbcd8559b
Maury Hill <maury.hill@gmal.com>
2022-03-23 10:36:12 -0500

    get rid right arrow access to nav layer

--- keyboards/kinesis/keymaps/maury/config.h
+++ keyboards/kinesis/keymaps/maury/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 // place overrides here
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
