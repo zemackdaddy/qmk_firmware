684dc1bbddf7411c54c96fa8044398d285e8dbe8
Maury Hill <maury.hill@gmail.com>
2022-06-25 11:43:21 -0500

    mod: swap bspc/spc; +esc; bottom: alt/pgup

--- keyboards/kinesis/keymaps/maury/keymap.c
+++ keyboards/kinesis/keymaps/maury/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                   KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
            KC_RGUI,  KC_RCTL, 
-           KC_LALT,
+           LALT_T(KC_PGUP),
            KC_PGDN,KC_ENTER ,  LT(MOD, KC_SPC)
     ),
 
@@ -101,23 +101,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MOD] = LAYOUT(
            KC_ESC, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
-           KC_TAB, KC_Q   ,RCS(KC_TAB)   ,RCTL(KC_T)   ,RCTL(KC_TAB)   ,KC_T   , 
+           KC_TAB, KC_ESC   ,RCS(KC_TAB)   ,RCTL(KC_T)   ,RCTL(KC_TAB)   ,KC_T   ,
            KC_CAPS, KC_A   ,KC_S   ,ALT_TAB   ,KC_F   ,KC_G   ,
            KC_LSFT,KC_Z   ,KC_X   , RCTL(KC_W)  , KC_V   ,KC_B   ,
                    KC_GRV ,KC_INS ,KC_LEFT, KC_RGHT,
 			                            KC_LCTL,  KC_LALT,
                                                         KC_HOME,
-                                  KC_SPC ,KC_DEL , KC_END ,
+                                  KC_BSPC ,KC_DEL , KC_END ,
 
   KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SLCK  , KC_PAUS, KC_FN0, KC_FN0,
 	KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
-	KC_PGUP   ,KC_U   ,KC_UP   ,KC_O   ,KC_P   ,KC_BSLS,
+	KC_PGUP   ,KC_HOME   ,KC_UP   ,KC_END   ,KC_P   ,KC_BSLS,
 	KC_PGDN   ,KC_LEFT   ,KC_DOWN   ,KC_RGHT   ,KC_SCLN,  KC_QUOT,
 	KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                   KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
            KC_RGUI,  KC_RCTL, 
            KC_PGUP,
-           KC_PGDN,KC_ENTER ,KC_BSPC
+           KC_PGDN,KC_ENTER ,KC_SPC
       	   ),
 
 [CLEAN] = LAYOUT(
