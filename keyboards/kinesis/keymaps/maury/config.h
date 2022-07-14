21f0bf3f58139d6755f026282238d5dfa64b804c
Maury Hill <maury.hill@gmail.com>
2022-07-13 19:10:13 -0500

    adjust the tapping term and permissive hold

--- keyboards/kinesis/keymaps/maury/config.h
+++ keyboards/kinesis/keymaps/maury/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // place overrides here
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define PERMISSIVE_HOLD
